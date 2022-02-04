31919208525ab7ee0285421ec3d6fdf69b0de1e7
Mike Cheuk <58591823+MikeCheuk@users.noreply.github.com>
2022-02-04 15:36:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
 import random
 import time
-import signal
-from beacontools.scanner import Monitor, HCIVersion
-
-beacon_packets = []
-
-def callback(bt_addr, rssi, packet, additional_info):
-    print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-    beacon_packets.append([bt_addr , rssi])
-    print(beacon_packets)
-    beacon.clear()
-    print(' ')
-
-#scan from all beacons
-monitor = Monitor(callback,
-                  bt_device_id = 0,
-                  device_filter = None,
-                  packet_filter = None,
-                  scan_parameters= {}
-                  )
-
-monitor.get_hci_version = lambda: HCIVersion.BT_CORE_SPEC_4_2
-monitor.start()
-
-signal.pause()
 
 #0. initialization (global variables)
 
