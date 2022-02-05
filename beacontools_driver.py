a33aade34d8f5c3259aca5958b53239ce676f87e
Mike Cheuk <58591823+MikeCheuk@users.noreply.github.com>
2022-02-05 18:04:01 +0800

    Update beacontools_driver.py

--- beacontools_driver.py
+++ beacontools_driver.py
@@ -5,9 +5,11 @@ def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
     beacon_packets.append([bt_addr , rssi])
     print(beacon_packets)
-    beacon.clear()
-    print(' ')
-    
+    print(' ')    
+    if len(beacon_packets) == 15:
+        beacon_packets.clear()
+        
+
 #scan from all beacons
 monitor = Monitor(callback,
                   bt_device_id = 0,
@@ -16,11 +18,8 @@ monitor = Monitor(callback,
                   scan_parameters= {}
                  )
 
-def main():
-    monitor.get_hci_version = lambda: HCIVersion.BT_CORE_SPEC_4_2
-    monitor.start()
-    signal.pause()
 
+monitor.get_hci_version = lambda: HCIVersion.BT_CORE_SPEC_4_2
+monitor.start()
+signal.pause()
 
-if __name__ == '__main__':
-    main()
