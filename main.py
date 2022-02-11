03b232d57659bf8bb88c606b05f8cd4fb49c8d13
Mike Cheuk <58591823+MikeCheuk@users.noreply.github.com>
2022-02-11 15:10:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,9 @@ sample_size = 5
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
     beacon_packets.append([bt_addr , rssi])
-    print(beacon_packets)
-    print(' ')    
     if len(beacon_packets) == packet_size:
         beacon_packets.pop(0)
         
-
 #scan from all beacons
 monitor = Monitor(callback,
                   bt_device_id = 0,
@@ -29,7 +26,6 @@ monitor = Monitor(callback,
                   packet_filter = None,
                   scan_parameters= {}
                  )
-
 
 monitor.get_hci_version = lambda: HCIVersion.BT_CORE_SPEC_4_2
 monitor.start()
@@ -139,6 +135,7 @@ print('Initializing... Please wait... ')
 time.sleep(3)
 
 run = True
+
 while run:
 
     #1. Beacon packet extraction
