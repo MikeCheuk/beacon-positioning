e246e21fc7e1ce4e1676ade6263c3a9a94a6b31c
Mike Cheuk <58591823+MikeCheuk@users.noreply.github.com>
2022-02-11 14:56:15 +0800

    Update beacontools_driver.py

--- beacontools_driver.py
+++ beacontools_driver.py
@@ -1,13 +1,15 @@
 import signal
 from beacontools.scanner import Monitor, HCIVersion
 
+packet_size = 15
+
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
     beacon_packets.append([bt_addr , rssi])
     print(beacon_packets)
     print(' ')    
-    if len(beacon_packets) == 15:
-        beacon_packets.clear()
+    if len(beacon_packets) == packet_size:
+        beacon_packets.pop(0)
         
 
 #scan from all beacons
